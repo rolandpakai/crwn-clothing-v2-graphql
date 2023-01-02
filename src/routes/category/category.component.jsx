446d8886bb56c4e2c4b981f1c8e50d109b0df9f1
rolandpakai <roland.pakai@liferay.com>
2023-01-02 15:37:34 +0100

    Add loading condition

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,14 +2,13 @@ import { useContext, useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 
 import ProductCard from '../../components/product-card/product-card.component';
-
 import { CategoriesContext } from '../../contexts/categories.context';
-
 import { CategoryContainer, Title } from './category.styles';
+import Spinner from '../../components/spinner/spinner.component';
 
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
+  const { categoriesMap, loading } = useContext(CategoriesContext);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -18,13 +17,19 @@ const Category = () => {
 
   return (
     <Fragment>
-      <Title>{category.toUpperCase()}</Title>
-      <CategoryContainer>
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </CategoryContainer>
+      {
+        loading ? <Spinner /> : (
+          <Fragment>
+            <Title>{category.toUpperCase()}</Title>
+            <CategoryContainer>
+              {products &&
+                products.map((product) => (
+                  <ProductCard key={product.id} product={product} />
+                ))}
+            </CategoryContainer>
+          </Fragment>
+        )
+      }
     </Fragment>
   );
 };
